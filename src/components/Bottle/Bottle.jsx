2b40f454076b80b2e2965a8dc69cc4fb0f700b26
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-27 00:15:25 +0600

    purchase button functionality

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,12 +1,13 @@
 import './Bottle.css'
-const Bottle = ({ bottle }) => {
-    const { name,img, price } = bottle;
+const Bottle = ({ bottle, handleAddCard }) => {
+    const { name, img, price } = bottle;
 
     return (
-        <div className = "bottle">
+        <div className="bottle">
             <h3>Bottle Name: {name}</h3>
             <img className="bottle-img" src={img} alt="" />
             <h4>Bottle Price: {price}</h4>
+            <button onClick={() => handleAddCard(bottle)}>Purchase</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -5,22 +5,31 @@ import './Bottles.css'
 
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('bottles.json')
             .then(res => res.json())
             .then(data => setBottles(data))
     }, [])
+
+    const handleAddCard = bottle => {
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+    }
+
     return (
         <div>
             <h3>Bottles Here</h3>
             <h4>Bottle Count: {bottles.length}</h4>
+            <h4>Cart: {cart.length}</h4>
             <div className="grid-display">
-            {
-                bottles.map(bottle => <Bottle
-                    bottle={bottle}
-                    key={bottle.id}
-                ></Bottle>)
-            }
+                {
+                    bottles.map(bottle => <Bottle
+                        bottle={bottle}
+                        key={bottle.id}
+                        handleAddCard={handleAddCard}
+                    ></Bottle>)
+                }
             </div>
         </div>
     );
